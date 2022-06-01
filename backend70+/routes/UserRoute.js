f075f12b496d504950ef68330f3fbc2ea0ebf1e6
dagamed131 <104123953+dagamed131@users.noreply.github.com>
2022-06-01 10:35:19 +0600

    Delete backend70+ directory

--- backend70+/routes/UserRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const UserController = require('../controllers/UserController')
-const router = express.Router();
-router.post('/user/register', UserController.register);
-router.post('/user/login', UserController.login);
-//router.get('/auth/google', UserController.authGoogle);
-module.exports = router
